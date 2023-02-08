cd7e111a20aafbc061b94e1684e91d62ffb3cbb2
NehaBardeDUKE <110474064+NehaBardeDUKE@users.noreply.github.com>
2023-02-08 13:25:48 -0500

    Update changes

---
+++ redact_ids/src/main.rs
@@ -0,0 +1,29 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(
+    version = "1.0",
+    author = "Neha Barde",
+    about = "A Redaction micro service"
+)]
+struct Cli {
+    #[clap(subcommand)]
+    //command: Option<Commands>,
+    command: Option<Commands>,
+}
+
+#[derive(Parser)]
+enum Commands {
+    #[clap(version = "1.0", author = "Neha Barde")]
+    Start,
+}
+
+fn main() {
+    let args = Cli::parse();
+    match args.command {
+        Some(Commands::Start) => {
+            redact_ids::data();
+        }
+        None => println!("No subcommand was used"),
+    }
+}
